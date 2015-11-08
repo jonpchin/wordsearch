499fb2b6164acc5d2f8487b4add18835430f8cfd
Richie Wales <richard_wales@knights.ucf.edu>
2015-11-07 19:22:33 -0500

    Created project with basic UI layout and scrabble dictionary

---
+++ Scrabble/Scrabble/SearchWord.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Scrabble
+{
+    class SearchWord
+    {
+        private Dictionary<string, int> wordList; //Holds words defined in text file
+        public SearchWord()
+        {
+            wordList = new Dictionary<string, int>();
+            MakeDictionary();
+        }
+
+        public void MakeDictionary()
+        {
+            var data = File.ReadAllLines("Content/words.txt"); //Reads in all data from file
+
+            for (int i = 0; i < data.Length; i++)              //Adds to Dictionary
+                wordList.Add(data[i], i);
+
+        }
+
+        public bool ValidWord(string word) //If word in dictionary, return true, else false
+        {
+            return (wordList.ContainsKey(word));
+        }
+    }
+}
